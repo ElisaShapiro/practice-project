2f773af43b8024b3f5c79bd39a6567feb88b85d0
Elisa Shapiro <elisashapiro@gmail.com>
2021-09-18 16:22:48 -0400

    backend validation all but email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: :true
-    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
